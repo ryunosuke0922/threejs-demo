c68ffea2d2a043f2d98607633c9c0251b4220c2c
ryunosuke0922 <ryumaeda0922@gmail.com>
2020-07-30 04:57:58 +0900

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const outputPath = path.resolve(__dirname, "dist");
 
-module.exports -
-  {
-    entry: "./src/index.js",
-    output: {
-      filename: "main.js",
-      path: outputPath,
-    },
-  };
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: outputPath,
+  },
+  devServer: {
+    contentBase: outputPath,
+  },
+};
